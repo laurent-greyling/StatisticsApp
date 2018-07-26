5b76047a5c5f5609e3840619aff103a78879bd92
Laurent Greyling <l.greyling@niposoftware.com>
2018-07-26 15:25:10 +0200

    Start add to sqlite table...

---
+++ Nfield.Stats/Nfield.Stats/Nfield.Stats/Utilities/AddLocalData.cs
@@ -0,0 +1,17 @@
+﻿using Nfield.Stats.Entities;
+using SQLite;
+using Xamarin.Forms;
+
+namespace Nfield.Stats.Utilities
+{
+    public class AddLocalData
+    {
+        private SQLiteConnection _connection;
+
+        public AddLocalData()
+        {
+            _connection = DependencyService.Get<ISQLite>().GetConnection();
+            _connection.CreateTable<Fake>();
+        }
+    }
+}
