3ad319854909c0a3b4f5862b540dae5595ea3909
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2018-08-02 21:26:01 +0200

    get surveys

---
+++ Nfield.Stats/Nfield.Stats/Nfield.Stats/Entities/SurveyDetailsEntity.cs
@@ -0,0 +1,22 @@
+﻿using SQLite;
+
+namespace Nfield.Stats.Entities
+{
+    public class SurveyDetailsEntity
+    {
+        [PrimaryKey]
+        public string SurveyId { get; set; }
+        public string SurveyName { get; set; }
+        public string ClientName { get; set; }
+        public string SurveyType { get; set; }
+        public string Description { get; set; }
+        public string QuestionnaireMD5 { get; set; }
+        public string InterviewerInstruction { get; set; }
+
+        public string Icon { get; set; }
+        public string Image { get; set; }
+
+        public bool IsFavourite { get; set; }
+        public string SuccessFulCount { get; set; }
+    }
+}
---
+++ Nfield.Stats/Nfield.Stats/Nfield.Stats/ViewModels/GetSurveysViewModel.cs
@@ -0,0 +1,56 @@
+﻿using Nfield.Stats.Models;
+using Nfield.Stats.Utilities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Xamarin.Forms;
+using Nfield.Stats.Services;
+
+namespace Nfield.Stats.ViewModels
+{
+    public class GetSurveysViewModel : INotifyPropertyChanged
+    {
+        public NotifyTaskCompletion<string> _surveysList { get; set; }
+
+        public NotifyTaskCompletion<string> SurveysList
+        {
+            get
+            {
+                return _surveysList;
+            }
+            set
+            {
+                if (_surveysList != value)
+                {
+                    _surveysList = value;
+                    OnPropertyChanged("SurveysList");
+                }
+            }
+        }
+
+        public GetSurveysViewModel(string authToken)
+        {
+            var restService = DependencyService
+            .Get<IRest>();
+
+            var serverService = DependencyService
+                .Get<INfieldServer>();
+
+            var nfieldServer = serverService
+                .Get()
+                .ServerDetails
+                .NfieldServer;
+
+            SurveysList = new NotifyTaskCompletion<string>(restService.GetAsync($"{nfieldServer}/v1/Surveys", authToken));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
