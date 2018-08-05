86f33b8db3dfe3dff4bf0c5044e28cdd82c7d362
laurent.greyling@gmail.com <laurent.greyling@gmail.com>
2018-08-05 07:54:30 +0200

    Start create survey service

---
+++ Nfield.Stats/Nfield.Stats/Nfield.Stats/Services/ISurveyService.cs
@@ -0,0 +1,15 @@
+﻿using Nfield.Stats.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Nfield.Stats.Services
+{
+    interface ISurveyService
+    {
+        SurveyDetails Get();
+
+        IEnumerable<SurveyDetails> GetList();
+
+        Task SaveAsync(string authToken);
+    }
+}
---
+++ Nfield.Stats/Nfield.Stats/Nfield.Stats/Services/SurveyService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Nfield.Stats.Models;
+using Nfield.Stats.Utilities;
+using Xamarin.Forms;
+
+namespace Nfield.Stats.Services
+{
+    public class SurveyService : ISurveyService
+    {
+        IRest _rest;
+        readonly INfieldServer _server;
+        readonly ISqliteService<SurveyDetails> _sqlite;
+
+        public SurveyService()
+        {
+            _rest = DependencyService
+            .Get<IRest>();
+
+            _server = DependencyService
+                .Get<INfieldServer>();
+
+            _sqlite = DependencyService
+                .Get<ISqliteService<SurveyDetails>>();
+        }
+
+        public SurveyDetails Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveAsync(string authToken)
+        {
+            var currentSurveys = GetList();
+            var surveysList = await _rest.GetAsync($"{_server}/v1/Surveys", authToken);
+            var serverSurveys = JsonConvert.DeserializeObject<List<SurveyDetails>>(surveysList);
+            var surveys = serverSurveys.Except(currentSurveys).ToList();
+            _sqlite.AddRange(surveys);
+        }
+
+        public IEnumerable<SurveyDetails> GetList()
+        {
+            return _sqlite.Get();
+        }
+    }
+}
