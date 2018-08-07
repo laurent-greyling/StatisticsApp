f0a0450b0f3713feac59ef063a479452918a024a
Laurent Greyling <l.greyling@niposoftware.com>
2018-08-07 15:24:37 +0200

    search for a survey

--- Nfield.Stats/Nfield.Stats/Nfield.Stats/Views/SurveysListPage.xaml.cs
+++ Nfield.Stats/Nfield.Stats/Nfield.Stats/Views/SurveysListPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Nfield.Stats.Models;
 using Nfield.Stats.ViewModels;
-
+using System;
+using System.Linq;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,12 +13,40 @@ namespace Nfield.Stats.Views
         public GetSurveysViewModel SurveysList { get; set; }
 		public SurveysListPage (AuthorizationModel accessToken)
 		{
-            SurveysList = new GetSurveysViewModel(accessToken.AuthenticationToken);
+            GetSurveys(accessToken.AuthenticationToken);
 
             InitializeComponent ();
 
             BindingContext = SurveysList;
 
         }
-	}
+
+        private void GetSurveys(string authToken)
+        {
+            try
+            {
+                SurveysList = new GetSurveysViewModel(authToken);
+            }
+            catch (Exception)
+            {
+                DisplayAlert("No Surveys", "Could not retrieve survey information", "OK");
+            }
+        }
+
+        private void Tapped_Search()
+        {
+            SearchBar.Focus();
+            SearchBar.IsVisible = !SearchBar.IsVisible ? true : false;
+        }
+
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            SurveyList.ItemsSource = string.IsNullOrWhiteSpace(e.NewTextValue)
+                ? SurveyList.ItemsSource = SurveysList.SurveysList.Result
+                : SurveyList.ItemsSource = SurveysList.SurveysList
+                                           .Result
+                                           .Where(n => n.SurveyName.ToLowerInvariant()
+                                                   .Contains(e.NewTextValue.ToLowerInvariant()));
+        }
+    }
 }
